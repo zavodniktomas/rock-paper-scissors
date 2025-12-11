4a5f5e488b25342a475d7d10a3fa80b6f6ecfb01
zavodniktomas <zavodniktomas95@gmail.com>
2025-12-11 10:32:00 +0100

    add playRound logic

--- script.js
+++ script.js
@@ -10,14 +10,34 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let choice = prompt("Choose rock, paper or scissors.")
 
-    return choice.toUpperCase ;
+    return choice.toUpperCase();
 }
 
+let humanScore = 0;
+let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection === computerSelection) {
+        console.log("It is a TIE!")
+    } else if ((humanSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (humanSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (humanSelection === 'PAPER' && computerSelection === 'ROCK')) {
+        console.log("You WIN!")
+        humanScore++;
+    }
+    else {
+        console.log("You LOSE!")
+        computerScore++;
+    }
+}
 
-console.log("Computer choice " + getComputerChoice());
-console.log("Human choice " + getHumanChoice());
+playRound(humanSelection, computerSelection)
+
+console.log("Computer choice " + computerSelection + " score " + computerScore);
+console.log("Human choice " + humanSelection + " score " + humanScore);
