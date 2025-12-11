2dd4fad14527a58e000fd51e19167fff31205f59
zavodniktomas <zavodniktomas95@gmail.com>
2025-12-11 10:03:24 +0100

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "ROCK";
+        case 1:
+            return "PAPER";
+        case 2:
+            return "SCISSORS";
+    }
+}
+
+
+console.log(getComputerChoice());
